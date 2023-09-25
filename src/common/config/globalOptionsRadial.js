39ecce4479094d65aa85c43defa706b583abf7dc
Brian Patrick <brian.patrick3@kyndryl.com>
2023-09-25 14:52:50 -0500

    fix: third party type tooltip colors

--- src/common/config/globalOptionsRadial.js
+++ src/common/config/globalOptionsRadial.js
@@ -1,5 +1,25 @@
 const defaultConfig = (ctx) => {
-  return {};
+  return {
+    plugins: {
+      tooltip: {
+        callbacks: {
+          labelColor: function (context) {
+            const PerDatapointColors = Array.isArray(
+              context.dataset.backgroundColor
+            );
+
+            return {
+              borderColor: context.dataset.borderColor,
+              backgroundColor: PerDatapointColors
+                ? context.dataset.backgroundColor[context.dataIndex]
+                : context.dataset.backgroundColor,
+              borderRadius: 2,
+            };
+          },
+        },
+      },
+    },
+  };
 };
 
 export default defaultConfig;
