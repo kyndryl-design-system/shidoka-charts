9f4fe1f461b18d11550e2b27dd79e6c28e3c9c52
Brian Patrick <brian.patrick3@kyndryl.com>
2023-09-27 10:48:42 -0500

    docs: update grid examples

--- src/stories/Grid.stories.js
+++ src/stories/Grid.stories.js
@@ -1,224 +1,215 @@
 import { html } from 'lit';
+import '../components/chart';
 
 export default {
   title: 'Patterns/Grid',
-  decorators: [
-    (story) =>
-      html`
-        <style>
-          .grid-examples .kd-grid {
-            margin-top: 32px;
-            margin-bottom: 32px;
-          }
-        </style>
-        ${story()}
-      `,
-  ],
 };
 
-const args = {};
+export const Full = {
+  render: () => {
+    return html` <div class="grid-examples">
+      <div class="kd-grid">
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-8 kd-grid__col--lg-12">
+          <kd-chart
+            type="bar"
+            chartTitle="Bar Chart"
+            description="Full Example"
+            .labels=${['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
+            .datasets=${[
+              {
+                label: 'Dataset 1',
+                data: [12, 19, 3, 5, 2, 3],
+              },
+              {
+                label: 'Dataset 2',
+                data: [8, 15, 7, 9, 6, 13],
+              },
+            ]}
+            .options=${{
+              scales: {
+                x: {
+                  title: {
+                    text: 'Color',
+                  },
+                },
+                y: {
+                  title: {
+                    text: 'Votes',
+                  },
+                },
+              },
+            }}
+          ></kd-chart>
+        </div>
+      </div>
+    </div>`;
+  },
+};
+export const Half = {
+  render: () => {
+    return html` <div class="kd-grid">
+      <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-6">
+        <kd-chart
+          type="bar"
+          chartTitle="Bar Chart"
+          description="Half Example"
+          .labels=${['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
+          .datasets=${[
+            {
+              label: 'Dataset 1',
+              data: [12, 19, 3, 5, 2, 3],
+            },
+            {
+              label: 'Dataset 2',
+              data: [8, 15, 7, 9, 6, 13],
+            },
+          ]}
+          .options=${{
+            scales: {
+              x: {
+                title: {
+                  text: 'Color',
+                },
+              },
+              y: {
+                title: {
+                  text: 'Votes',
+                },
+              },
+            },
+          }}
+        ></kd-chart>
+      </div>
 
-export const Grid = {
-  args,
-  render: (args) => {
+      <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-6">
+        <kd-chart
+          type="doughnut"
+          chartTitle="Doughnut Chart"
+          description="Half Example"
+          .labels=${['Blue', 'Red', 'Orange', 'Yellow', 'Green', 'Purple']}
+          .datasets=${[
+            {
+              label: 'Dataset 1',
+              data: [12, 19, 3, 5, 2, 3],
+            },
+          ]}
+          .options=${{
+            scales: {
+              x: {
+                title: {
+                  text: 'Color',
+                },
+              },
+              y: {
+                title: {
+                  text: 'Votes',
+                },
+              },
+            },
+          }}
+        ></kd-chart>
+      </div>
+    </div>`;
+  },
+};
+
+export const Third = {
+  render: () => {
     return html`
-      <div class="grid-examples">
-        <div class="kd-grid">
-          <div
-            class="kd-grid__col--sm-4 kd-grid__col--md-8 kd-grid__col--lg-12"
-          >
-            <kd-chart
-              type="bar"
-              chartTitle="Bar Chart"
-              description="Full Example"
-              .labels=${['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
-              .datasets=${[
-                {
-                  label: 'Dataset 1',
-                  data: [12, 19, 3, 5, 2, 3],
-                },
-                {
-                  label: 'Dataset 2',
-                  data: [8, 15, 7, 9, 6, 13],
-                },
-              ]}
-              .options=${{
-                scales: {
-                  x: {
-                    title: {
-                      text: 'Color',
-                    },
-                  },
-                  y: {
-                    title: {
-                      text: 'Votes',
-                    },
+      <div class="kd-grid">
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-4">
+          <kd-chart
+            type="bar"
+            chartTitle="Bar Chart"
+            description="Third Example"
+            .labels=${['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
+            .datasets=${[
+              {
+                label: 'Dataset 1',
+                data: [12, 19, 3, 5, 2, 3],
+              },
+              {
+                label: 'Dataset 2',
+                data: [8, 15, 7, 9, 6, 13],
+              },
+            ]}
+            .options=${{
+              scales: {
+                x: {
+                  title: {
+                    text: 'Color',
                   },
                 },
-              }}
-            ></kd-chart>
-          </div>
+                y: {
+                  title: {
+                    text: 'Votes',
+                  },
+                },
+              },
+            }}
+          ></kd-chart>
         </div>
 
-        <div class="kd-grid">
-          <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-6">
-            <kd-chart
-              type="bar"
-              chartTitle="Bar Chart"
-              description="Half Example"
-              .labels=${['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
-              .datasets=${[
-                {
-                  label: 'Dataset 1',
-                  data: [12, 19, 3, 5, 2, 3],
-                },
-                {
-                  label: 'Dataset 2',
-                  data: [8, 15, 7, 9, 6, 13],
-                },
-              ]}
-              .options=${{
-                scales: {
-                  x: {
-                    title: {
-                      text: 'Color',
-                    },
-                  },
-                  y: {
-                    title: {
-                      text: 'Votes',
-                    },
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-4">
+          <kd-chart
+            type="doughnut"
+            chartTitle="Doughnut Chart"
+            description="Third Example"
+            .labels=${['Blue', 'Red', 'Orange', 'Yellow', 'Green', 'Purple']}
+            .datasets=${[
+              {
+                label: 'Dataset 1',
+                data: [12, 19, 3, 5, 2, 3],
+              },
+            ]}
+            .options=${{
+              scales: {
+                x: {
+                  title: {
+                    text: 'Color',
                   },
                 },
-              }}
-            ></kd-chart>
-          </div>
-
-          <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-6">
-            <kd-chart
-              type="doughnut"
-              chartTitle="Doughnut Chart"
-              description="Half Example"
-              .labels=${['Blue', 'Red', 'Orange', 'Yellow', 'Green', 'Purple']}
-              .datasets=${[
-                {
-                  label: 'Dataset 1',
-                  data: [12, 19, 3, 5, 2, 3],
-                },
-              ]}
-              .options=${{
-                scales: {
-                  x: {
-                    title: {
-                      text: 'Color',
-                    },
-                  },
-                  y: {
-                    title: {
-                      text: 'Votes',
-                    },
+                y: {
+                  title: {
+                    text: 'Votes',
                   },
                 },
-              }}
-            ></kd-chart>
-          </div>
+              },
+            }}
+          ></kd-chart>
         </div>
 
-        <div class="kd-grid">
-          <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-4">
-            <kd-chart
-              type="bar"
-              chartTitle="Bar Chart"
-              description="Third Example"
-              .labels=${['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
-              .datasets=${[
-                {
-                  label: 'Dataset 1',
-                  data: [12, 19, 3, 5, 2, 3],
-                },
-                {
-                  label: 'Dataset 2',
-                  data: [8, 15, 7, 9, 6, 13],
-                },
-              ]}
-              .options=${{
-                scales: {
-                  x: {
-                    title: {
-                      text: 'Color',
-                    },
-                  },
-                  y: {
-                    title: {
-                      text: 'Votes',
-                    },
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-4">
+          <kd-chart
+            type="line"
+            chartTitle="Line Chart"
+            description="Third Example"
+            .labels=${['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
+            .datasets=${[
+              {
+                label: 'Dataset 1',
+                data: [12, 19, 3, 5, 2, 3],
+              },
+              {
+                label: 'Dataset 2',
+                data: [8, 15, 7, 9, 6, 13],
+              },
+            ]}
+            .options=${{
+              scales: {
+                x: {
+                  title: {
+                    text: 'Color',
                   },
                 },
-              }}
-            ></kd-chart>
-          </div>
-
-          <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-4">
-            <kd-chart
-              type="doughnut"
-              chartTitle="Doughnut Chart"
-              description="Third Example"
-              .labels=${['Blue', 'Red', 'Orange', 'Yellow', 'Green', 'Purple']}
-              .datasets=${[
-                {
-                  label: 'Dataset 1',
-                  data: [12, 19, 3, 5, 2, 3],
-                },
-              ]}
-              .options=${{
-                scales: {
-                  x: {
-                    title: {
-                      text: 'Color',
-                    },
-                  },
-                  y: {
-                    title: {
-                      text: 'Votes',
-                    },
+                y: {
+                  title: {
+                    text: 'Votes',
                   },
                 },
-              }}
-            ></kd-chart>
-          </div>
-
-          <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-4">
-            <kd-chart
-              type="line"
-              chartTitle="Line Chart"
-              description="Third Example"
-              .labels=${['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
-              .datasets=${[
-                {
-                  label: 'Dataset 1',
-                  data: [12, 19, 3, 5, 2, 3],
-                },
-                {
-                  label: 'Dataset 2',
-                  data: [8, 15, 7, 9, 6, 13],
-                },
-              ]}
-              .options=${{
-                scales: {
-                  x: {
-                    title: {
-                      text: 'Color',
-                    },
-                  },
-                  y: {
-                    title: {
-                      text: 'Votes',
-                    },
-                  },
-                },
-              }}
-            ></kd-chart>
-          </div>
+              },
+            }}
+          ></kd-chart>
         </div>
       </div>
     `;
