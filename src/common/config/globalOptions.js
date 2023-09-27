b83862072b72ef109f3c823a40a3620ff77e7fdb
Brian Patrick <brian.patrick3@kyndryl.com>
2023-09-27 15:28:37 -0500

    chore: add potential legend hover functions

--- src/common/config/globalOptions.js
+++ src/common/config/globalOptions.js
@@ -24,6 +24,8 @@ const defaultConfig = (ctx) => {
           useBorderRadius: true,
           padding: 8,
         },
+        // onHover: handleLegendHover,
+        // onLeave: handleLegendLeave,
       },
       tooltip: {
         backgroundColor: TooltipBgColor,
@@ -54,3 +56,88 @@ const defaultConfig = (ctx) => {
 };
 
 export default defaultConfig;
+
+/**
+ * The function `handleLegendHover` updates the background and border colors of a chart legend item
+ * when it is hovered over.
+ * @param e - The `e` parameter is an event object that represents the event that triggered the
+ * function. It can be used to access information about the event, such as the target element or the
+ * event type.
+ * @param item - The `item` parameter represents the legend item that was hovered over. It contains
+ * information about the dataset index and the index of the hovered item within that dataset.
+ * @param legend - The `legend` parameter is the legend object of a chart. It contains information
+ * about the chart's legend, such as the labels and colors of the legend items.
+ */
+const handleLegendHover = (e, item, legend) => {
+  const DatasetIndex = item.datasetIndex || 0;
+  const Datasets = legend.chart.data.datasets;
+  const Dataset = Datasets[DatasetIndex];
+
+  if (Array.isArray(Dataset.backgroundColor)) {
+    Dataset.backgroundColor.forEach((color, index, colors) => {
+      colors[index] =
+        index === item.index || color.length === 9 ? color : color + '4D';
+    });
+  } else {
+    Datasets.forEach((dataset, index) => {
+      const backgroundColor = dataset.backgroundColor;
+      const borderColor = dataset.borderColor;
+
+      if (backgroundColor) {
+        dataset.backgroundColor =
+          index === DatasetIndex || backgroundColor.length === 9
+            ? backgroundColor
+            : backgroundColor + '4D';
+      }
+
+      if (borderColor) {
+        dataset.borderColor =
+          index === DatasetIndex || borderColor.length === 9
+            ? borderColor
+            : borderColor + '4D';
+      }
+    });
+  }
+
+  legend.chart.update();
+};
+
+/**
+ * The function `handleLegendLeave` updates the background and border colors of a chart legend when the
+ * mouse leaves the legend item.
+ * @param e - The event object that triggered the legend leave event.
+ * @param item - The `item` parameter represents the legend item that was interacted with. It contains
+ * information about the dataset index and other properties related to the legend item.
+ * @param legend - The `legend` parameter is the legend object of a chart. It contains information
+ * about the chart's legend, such as the labels and colors of the legend items.
+ */
+const handleLegendLeave = (e, item, legend) => {
+  const DatasetIndex = item.datasetIndex || 0;
+  const Datasets = legend.chart.data.datasets;
+  const Dataset = Datasets[DatasetIndex];
+
+  if (Array.isArray(Dataset.backgroundColor)) {
+    Dataset.backgroundColor.forEach((color, index, colors) => {
+      colors[index] = color.length === 9 ? color.slice(0, -2) : color;
+    });
+  } else {
+    Datasets.forEach((dataset, index) => {
+      const backgroundColor = dataset.backgroundColor;
+      const borderColor = dataset.borderColor;
+
+      if (backgroundColor) {
+        dataset.backgroundColor =
+          backgroundColor.length === 9
+            ? backgroundColor.slice(0, -2)
+            : backgroundColor;
+      }
+
+      if (borderColor) {
+        dataset.borderColor =
+          borderColor.length === 9 ? borderColor.slice(0, -2) : borderColor;
+      }
+    });
+  }
+
+  legend.chart.update();
+};
