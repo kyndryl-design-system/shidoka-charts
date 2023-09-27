0e9d7bf81aed0b4ecbe9b389a407939f4093c4a2
Brian Patrick <brian.patrick3@kyndryl.com>
2023-09-27 10:51:32 -0500

    docs: fix grid html

--- src/stories/Grid.stories.js
+++ src/stories/Grid.stories.js
@@ -7,52 +7,12 @@ export default {
 
 export const Full = {
   render: () => {
-    return html` <div class="grid-examples">
-      <div class="kd-grid">
-        <div class="kd-grid__col--sm-4 kd-grid__col--md-8 kd-grid__col--lg-12">
-          <kd-chart
-            type="bar"
-            chartTitle="Bar Chart"
-            description="Full Example"
-            .labels=${['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
-            .datasets=${[
-              {
-                label: 'Dataset 1',
-                data: [12, 19, 3, 5, 2, 3],
-              },
-              {
-                label: 'Dataset 2',
-                data: [8, 15, 7, 9, 6, 13],
-              },
-            ]}
-            .options=${{
-              scales: {
-                x: {
-                  title: {
-                    text: 'Color',
-                  },
-                },
-                y: {
-                  title: {
-                    text: 'Votes',
-                  },
-                },
-              },
-            }}
-          ></kd-chart>
-        </div>
-      </div>
-    </div>`;
-  },
-};
-export const Half = {
-  render: () => {
     return html` <div class="kd-grid">
-      <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-6">
+      <div class="kd-grid__col--sm-4 kd-grid__col--md-8 kd-grid__col--lg-12">
         <kd-chart
           type="bar"
           chartTitle="Bar Chart"
-          description="Half Example"
+          description="Full Example"
           .labels=${['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
           .datasets=${[
             {
@@ -80,36 +40,76 @@ export const Half = {
           }}
         ></kd-chart>
       </div>
-
-      <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-6">
-        <kd-chart
-          type="doughnut"
-          chartTitle="Doughnut Chart"
-          description="Half Example"
-          .labels=${['Blue', 'Red', 'Orange', 'Yellow', 'Green', 'Purple']}
-          .datasets=${[
-            {
-              label: 'Dataset 1',
-              data: [12, 19, 3, 5, 2, 3],
-            },
-          ]}
-          .options=${{
-            scales: {
-              x: {
-                title: {
-                  text: 'Color',
-                },
-              },
-              y: {
-                title: {
-                  text: 'Votes',
-                },
-              },
-            },
-          }}
-        ></kd-chart>
-      </div>
     </div>`;
+  },
+};
+export const Half = {
+  render: () => {
+    return html`
+      <div class="kd-grid">
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-6">
+          <kd-chart
+            type="bar"
+            chartTitle="Bar Chart"
+            description="Half Example"
+            .labels=${['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
+            .datasets=${[
+              {
+                label: 'Dataset 1',
+                data: [12, 19, 3, 5, 2, 3],
+              },
+              {
+                label: 'Dataset 2',
+                data: [8, 15, 7, 9, 6, 13],
+              },
+            ]}
+            .options=${{
+              scales: {
+                x: {
+                  title: {
+                    text: 'Color',
+                  },
+                },
+                y: {
+                  title: {
+                    text: 'Votes',
+                  },
+                },
+              },
+            }}
+          ></kd-chart>
+        </div>
+
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-6">
+          <kd-chart
+            type="doughnut"
+            chartTitle="Doughnut Chart"
+            description="Half Example"
+            .labels=${['Blue', 'Red', 'Orange', 'Yellow', 'Green', 'Purple']}
+            .datasets=${[
+              {
+                label: 'Dataset 1',
+                data: [12, 19, 3, 5, 2, 3],
+              },
+            ]}
+            .options=${{
+              scales: {
+                x: {
+                  title: {
+                    text: 'Color',
+                  },
+                },
+                y: {
+                  title: {
+                    text: 'Votes',
+                  },
+                },
+              },
+            }}
+          ></kd-chart>
+        </div>
+      </div>
+    `;
   },
 };
 
