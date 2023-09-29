4db467a53127f91018ba0af2013475b5d5fa50f0
Brian Patrick <brian.patrick3@kyndryl.com>
2023-09-29 11:34:24 -0500

    chore: treemap color experiment

--- src/common/config/chartTypes/treemap.js
+++ src/common/config/chartTypes/treemap.js
@@ -40,9 +40,54 @@ export const options = (ctx) => {
 };
 
 export const datasetOptions = (ctx, index) => {
+  // console.log(Groups);
+
   return {
     backgroundColor:
       colorPalettes[ctx.options.colorPalette || 'default'][0] + '80',
     borderColor: colorPalettes[ctx.options.colorPalette || 'default'][0],
+    // borderColor: function (context) {
+    //   return colorPalettes[ctx.options.colorPalette || 'default'][
+    //     getGroupColorIndex(context)
+    //   ];
+    // },
+    // backgroundColor: function (context) {
+    //   return (
+    //     colorPalettes[ctx.options.colorPalette || 'default'][
+    //       getGroupColorIndex(context)
+    //     ] + '80'
+    //   );
+    // },
   };
+};
+
+/**
+ * The function `getGroupColorIndex` returns the index of a group color based on the context provided.
+ * Works for grouped charts, not nested data charts yet.
+ * @param context - The `context` parameter is an object that contains information about the current
+ * context or state of the program. It is used to determine the group color index based on the dataset
+ * and data index.
+ * @returns the index of the group color for a given context.
+ */
+const getGroupColorIndex = (context) => {
+  const Dataset = context.dataset;
+  const DataIndex = context.dataIndex;
+  const GroupKey = Dataset.groups ? Dataset.groups[0] : null;
+  const Groups = [];
+
+  if (GroupKey && !Dataset.treeLeafKey) {
+    Dataset.tree.forEach((leaf) => {
+      if (!Groups.includes(leaf[GroupKey])) {
+        Groups.push(leaf[GroupKey]);
+      }
+    });
+  }
+
+  const Leaf = Dataset.data[DataIndex];
+  let index = 0;
+  if (Leaf) {
+    index = Groups.indexOf(Leaf._data[GroupKey]);
+  }
+
+  return index < 0 ? 0 : index;
 };
