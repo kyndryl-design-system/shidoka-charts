1a543e8a96bc2e398ab0eec797972146d2a02e54
will-barbee-kyndryl <will.barbee@kyndryl.com>
2025-11-10 10:31:41 -0600

    fix: lint
    Signed-off-by: will-barbee-kyndryl <will.barbee@kyndryl.com>

--- src/common/config/chartTypes/sankey.js
+++ src/common/config/chartTypes/sankey.js
@@ -1,4 +1,4 @@
-import { getComputedColorPalette } from '../colorPalettes';
+import { getComputedColorPalette, getColorPalette } from '../colorPalettes';
 import { getTokenThemeVal } from '@kyndryl-design-system/shidoka-foundation/common/helpers/color';
 
 export const type = 'sankey';
@@ -16,58 +16,65 @@ export const options = () => {
   };
 };
 
-export const datasetOptions = (ctx) => {
+export const datasetOptions = (ctx, index) => {
+  const unsafeKey = ctx && ctx.options && ctx.options.colorPalette;
   const paletteKey =
-    ctx && ctx.options && ctx.options.colorPalette
-      ? ctx.options.colorPalette
-      : 'categorical';
-  let palette;
-  try {
-    palette = getComputedColorPalette(paletteKey);
-  } catch (e) {
-    palette = getComputedColorPalette('categorical');
-  }
+    unsafeKey && getColorPalette(unsafeKey) ? unsafeKey : 'categorical';
+
+  const palette = getComputedColorPalette(paletteKey);
+
+  const buildNodeColorMap = (dataset) => {
+    if (dataset._nodeColorMap) return dataset._nodeColorMap;
+
+    const nodes = [];
+    (dataset.data || []).forEach((d) => {
+      if (!d) return;
+      if (d.from !== undefined && !nodes.includes(d.from)) nodes.push(d.from);
+      if (d.to !== undefined && !nodes.includes(d.to)) nodes.push(d.to);
+    });
+
+    const map = {};
+    nodes.forEach((n, i) => {
+      map[n] = palette[i % palette.length];
+    });
+
+    dataset._nodeColorMap = map;
+    return map;
+  };
 
   const getNodeColor = (dataset, key) => {
-    if (!dataset._nodeColorMap) {
-      const nodes = [];
-      (dataset.data || []).forEach((d) => {
-        if (d && d.from !== undefined && !nodes.includes(d.from))
-          nodes.push(d.from);
-        if (d && d.to !== undefined && !nodes.includes(d.to)) nodes.push(d.to);
-      });
-      const map = {};
-      nodes.forEach((n, i) => {
-        map[n] = palette[i % palette.length];
-      });
-      dataset._nodeColorMap = map;
-    }
-    return dataset._nodeColorMap[key] || palette[0];
+    if (!dataset) return palette[0];
+    const map = buildNodeColorMap(dataset);
+    return key !== undefined && map[key] ? map[key] : palette[0];
   };
 
   return {
     colorFrom: function (context) {
       const ds = context.dataset;
-      const dataIndex = context.dataIndex ?? 0;
-      const key = ds.data?.[dataIndex]?.from;
+      const dataIndex =
+        typeof context.dataIndex === 'number' ? context.dataIndex : 0;
+      const key = ds?.data?.[dataIndex]?.from;
       return getNodeColor(ds, key);
     },
     colorTo: function (context) {
       const ds = context.dataset;
-      const dataIndex = context.dataIndex ?? 0;
-      const key = ds.data?.[dataIndex]?.to;
+      const dataIndex =
+        typeof context.dataIndex === 'number' ? context.dataIndex : 0;
+      const key = ds?.data?.[dataIndex]?.to;
       return getNodeColor(ds, key);
     },
     hoverColorFrom: function (context) {
       const ds = context.dataset;
-      const dataIndex = context.dataIndex ?? 0;
-      const key = ds.data?.[dataIndex]?.from;
+      const dataIndex =
+        typeof context.dataIndex === 'number' ? context.dataIndex : 0;
+      const key = ds?.data?.[dataIndex]?.from;
       return getNodeColor(ds, key);
     },
     hoverColorTo: function (context) {
       const ds = context.dataset;
-      const dataIndex = context.dataIndex ?? 0;
-      const key = ds.data?.[dataIndex]?.to;
+      const dataIndex =
+        typeof context.dataIndex === 'number' ? context.dataIndex : 0;
+      const key = ds?.data?.[dataIndex]?.to;
       return getNodeColor(ds, key);
     },
     colorMode: 'gradient',
