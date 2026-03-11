60c0ec9fa345fb2eccafaa9a5b8191af0873ef9c
Brian Patrick <brian.patrick3@kyndryl.com>
2026-03-11 15:27:01 -0500

    docs: reorder sidebar
    Signed-off-by: Brian Patrick <brian.patrick3@kyndryl.com>

--- src/components/mermaid/mermaid.stories.js
+++ src/components/mermaid/mermaid.stories.js
@@ -3,7 +3,7 @@ import { html } from 'lit';
 import './index';
 
 export default {
-  title: 'Mermaid/Diagrams',
+  title: 'Mermaid/Mermaid Diagrams',
   component: 'kyn-mermaid',
   parameters: {
     design: {
